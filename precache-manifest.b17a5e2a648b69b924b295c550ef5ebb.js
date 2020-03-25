51b2f3a02d548c13081b8a6d80eb1be1b625f024
Marcus Italo <marcusitalo@outlook.com.br>
2020-03-25 16:53:53 -0300

    Updates

---
+++ precache-manifest.b17a5e2a648b69b924b295c550ef5ebb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "312341c602e32a16856d8f1c0505708f",
+    "url": "/browse-gifs/index.html"
+  },
+  {
+    "revision": "72179e84ab7e7f53adcd",
+    "url": "/browse-gifs/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "992eab4e4daa402cc7d5",
+    "url": "/browse-gifs/static/css/main.978f3bcb.chunk.css"
+  },
+  {
+    "revision": "72179e84ab7e7f53adcd",
+    "url": "/browse-gifs/static/js/2.e47512cc.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/browse-gifs/static/js/2.e47512cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "992eab4e4daa402cc7d5",
+    "url": "/browse-gifs/static/js/main.c2f11020.chunk.js"
+  },
+  {
+    "revision": "fe9ec49db0e33d02c798",
+    "url": "/browse-gifs/static/js/runtime-main.25780cd0.js"
+  },
+  {
+    "revision": "aa8f3e480ef09f68a3ba29d38526a54d",
+    "url": "/browse-gifs/static/media/background.aa8f3e48.jpg"
+  }
+]);
